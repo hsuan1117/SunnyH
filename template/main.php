5333979aab745bd909614f7c7cee81963cb97b0c
Xi-Plus <a2093064@yahoo.com.tw>
2015-04-14 12:58:26 +0800

    header exterior

--- template/main.php
+++ template/main.php
@@ -5,10 +5,12 @@ if(!defined("IN_SYSTEM"))
 
 <?php require("header.php"); ?>
 
+<center>
 <form method="POST">
 	Account/E-mail: <input type="text" name="account"><br>
 	Password: <input type="password" name="password"><br>
 	<input type="submit" value="login">
 </form>
+</center>
 
 <?php require("footer.php"); ?>
