5e31ab6493a418f16fba11af5a658a4193e06d9e
domen111 <domen.public@gmail.com>
2015-04-14 17:02:17 +0800

    edit sql function

--- func/sql.php
+++ func/sql.php
@@ -2,27 +2,33 @@
 if(!defined("IN_SYSTEM"))
 	die("Access Denied");
 
-function createPDO()
+function PDO_prepare($statement)
 {
 	global $config;
 	$dbconf = $config["DB"];
 	try{
-		return new PDO("mysql:host=$dbconf[host]; dbname=$dbconf[dbname]; charset=$dbconf[charset]", $dbconf["user"], $dbconf["pwd"]);
+		$db = new PDO("mysql:host=$dbconf[host]; dbname=$dbconf[dbname]; charset=$dbconf[charset]", $dbconf["user"], $dbconf["pwd"]);
 	}catch(PDOException $e){
 		exit("Error establishing DB connection: <br>" . $e->getMessage());
+	}
+	
+	$statement = str_replace("table:",$dbconf["tablepre"],$statement);
+	try{
+		return $db->prepare($statement);
+	}catch(PDOException $e){
+		die("SQL ERROR: " . $e->getMessage());
 	}
 }
 
 function checkPassword($account,$password) //return true: succeed, 0: wrong pass, -1: no such acct
 {
-	$db = createPDO();
+	$db = PDO_prepare("SELECT * FROM `table:account` WHERE `account`=?");
 	try{
-		$dbc = $db->prepare("SELECT * FROM `account` WHERE `account`=?");
-		$dbc->bindValue(1, $account, PDO::PARAM_STR);
-		$dbc->execute();
-		if($dbc->rowCount()<1)
+		$db->bindValue(1, $account, PDO::PARAM_STR);
+		$db->execute();
+		if($db->rowCount()<1)
 			return false;
-		$real_pass = $dbc->fetchAll()[0]["password"];
+		$real_pass = $db->fetchAll()[0]["password"];
 		return crypt($password,$real_pass) === $real_pass;
 	}catch(PDOException $e){
 		die("SQL ERROR: " . $e->getMessage());
