026847909c5165265867b2bf0f2b7e40fab626f3
domen111 <domen.public@gmail.com>
2015-04-15 09:49:03 +0800

    session cookie

--- func/sql.php
+++ func/sql.php
@@ -19,18 +19,3 @@ function PDO_prepare($statement)
 		die("SQL ERROR: " . $e->getMessage());
 	}
 }
-
-function checkPassword($account,$password) //return true: succeed, 0: wrong pass, -1: no such acct
-{
-	$db = PDO_prepare("SELECT * FROM `table:account` WHERE `account`=?");
-	try{
-		$db->bindValue(1, $account, PDO::PARAM_STR);
-		$db->execute();
-		if($db->rowCount()<1)
-			return false;
-		$real_pass = $db->fetchAll()[0]["password"];
-		return crypt($password,$real_pass) === $real_pass;
-	}catch(PDOException $e){
-		die("SQL ERROR: " . $e->getMessage());
-	}
-}
--- index.php
+++ index.php
@@ -1,15 +1,76 @@
 <?php
 require_once("system.php");
-if(isset($_POST["account"]) && isset($_POST["password"]))
-{
-	if(checkPassword($_POST["account"],$_POST["password"]) === true){
+if(isset($_POST["account"]) && isset($_POST["password"])){
+	$user = checkPassword($_POST["account"],$_POST["password"]);
+	if($user !== -1 && $user !== -2){
 		echo "login succeed";
+		
+		//set cookie
+		$hash = randomHash(32);
+		if($config["session"]["keep_login"]===false)
+			$expire = 0;
+		else
+			$expire = mktime().time()+60*60*24*$config["session"]["expire"];
+		setcookie("login",$hash,$expire,$config["session"]["cookie_path"],$config["session"]["domain"],false,true);
+		
+		//set DB
+		$db = PDO_prepare("INSERT INTO `table:session` (`id`, `expire`, `cookie`) VALUES (:id, DATE_ADD(CURDATE(),INTERVAL 30 DAY), :cookie)");
+		try{
+			$db->bindValue("id", $user["id"], PDO::PARAM_INT);
+			$db->bindValue("cookie", $hash, PDO::PARAM_STR);
+			$db->execute();
+		}catch(PDOException $e){
+			die("SQL ERROR: " . $e->getMessage());
+		}
+		
+		header('refresh: 0;url=');
 	}else{
 		$E["msg"] = "login failed";
 		require("template/main.php");
 	}
-}
-else
-{
+}else if(isset($_COOKIE["login"])){
+	//clear expired db data
+	$db = PDO_prepare("DELETE FROM `table:session` WHERE `expire`<CURDATE()");
+	$db->execute();
+	//check cookie
+	$db = PDO_prepare("SELECT `id` FROM `table:session` WHERE `cookie`=:cookie");
+	$db->bindValue("cookie", $_COOKIE["login"], PDO::PARAM_STR);
+	$db->execute();
+	if($db->rowCount()>0){
+		$uid = $db->fetchAll()[0]["id"];
+		echo $uid;
+	}else{
+		setcookie("login",null,-1,$config["session"]["cookie_path"],$config["session"]["domain"],false,true); //delete cookie
+		header('refresh: 0;url=');
+	}
+}else{
 	require("template/main.php");
+}
+
+function checkPassword($account,$password) //return value: success: user, no such acct: -1, wrong pass: -2
+{
+	$db = PDO_prepare("SELECT * FROM `table:account` WHERE `account`=?");
+	try{
+		$db->bindValue(1, $account, PDO::PARAM_STR);
+		$db->execute();
+		if($db->rowCount()<1)
+			return -1;
+		$data = $db->fetchAll()[0];
+		$real_pass = $data["password"];
+		if(crypt($password,$real_pass) !== $real_pass)
+			return -2;
+		return $data;
+	}catch(PDOException $e){
+		die("SQL ERROR: " . $e->getMessage());
+	}
+}
+
+function randomHash($len)
+{
+	$pattern = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	$ptn_len = strlen($pattern);
+	$hash = "";
+	for($i=0;$i<$len;$i++)
+		$hash .= $pattern[mt_rand(0,$ptn_len-1)];
+	return $hash;
 }
