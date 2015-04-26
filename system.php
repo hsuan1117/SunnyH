99e14a6c4c6057989a5b043ec81600ddaa426a22
Xi-Plus <a2093064@yahoo.com.tw>
2015-04-26 21:54:25 +0800

    move [get login info code] index.php -> system.php
    because header.php is needed in all pages.

--- system.php
+++ system.php
@@ -7,3 +7,16 @@ require_once("func/functions.php");
 # template variables
 $E = array();
 $E["msg"] = "";
+
+$uid = checklogin();
+if($uid!==false){
+	$E["login"] = true;
+	$db = PDO_prepare("SELECT * FROM `table:account` WHERE `id`=:id");
+	$db->bindValue("id", $uid, PDO::PARAM_INT);
+	$db->execute();
+	$data = $db->fetchAll()[0];
+	$E["nick"] = $data["nickname"];
+	$E["acct"] = $data["account"];
+}else {
+	$E["login"] = false;
+}
