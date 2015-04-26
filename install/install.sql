59ccec017951e2f180245c240c363e9f65637928
Xi-Plus <a2093064@yahoo.com.tw>
2015-04-26 12:50:48 +0800

    Debug crypt length

--- install/install.sql
+++ install/install.sql
@@ -7,7 +7,7 @@ if(!defined("IN_SYSTEM"))
 CREATE TABLE `<?=$tablepre?>account` (
 	`id` int AUTO_INCREMENT,
 	`account` varchar(30) NOT NULL UNIQUE,
-	`password` varchar(30) NOT NULL,
+	`password` varchar(34) NOT NULL,
 	`email` varchar(255) NOT NULL UNIQUE,
 	`nickname` text NOT NULL,
 	PRIMARY KEY (`id`)
