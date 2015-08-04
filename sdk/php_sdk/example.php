3f21ec7a3a59e19382eefe38b0cd19db39499c19
domen111 <domen.public@gmail.com>
2015-08-04 09:15:23 +0000

    PHP SDK Example Page

--- sdk/php_sdk/example.php
+++ sdk/php_sdk/example.php
@@ -1,7 +1,33 @@
 <?php
 include("func.php");
+login_system::$url = "http://".$_SERVER['HTTP_HOST']."/login/";
+$status = login_system::status();
+?>
 
-login_system::$url = "http://domen111.koding.io/login/";
-echo "<pre>";
-var_dump(login_system::status());
-echo "</pre>";
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>TFcis Login - PHP SDK example</title>
+</head>
+
+<body>
+	<?php if($status->login===false){ ?>
+		You haven't login yet. <br>
+		<a href="<?=$status->url?>">Login</a>
+	<?php } else { ?>
+		Id: <?=$status->data->id?> <br>
+		User Name: <?=$status->data->account?> <br>
+		E-mail: <?=$status->data->email?> <br>
+		Nickname: <?=$status->data->nickname?> <br>
+		<a href="<?=$status->url?>">Logout</a>
+	<?php } ?>
+	
+	<br><br><br><br>
+	Raw Data:
+	<pre><?php var_dump($status); ?></pre>
+</body>
+
+</html>
