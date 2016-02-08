39b9e907c6f15b5b0809fd5f20dd31b6c366fc22
Xi-Plus <huangxuanyuxiplus@gmail.com>
2016-02-08 14:37:48 +0800

    edit setting css

--- template/setting.php
+++ template/setting.php
@@ -7,12 +7,14 @@ if(!defined("IN_SYSTEM"))
 
 <center>
 <form method="POST">
-	Account: <?=@$E["info"]["account"]?><br>
-	Password: <input type="password" name="password"><br>
-	Confirm: <input type="password" name="password2"><br>
-	Nickname: <input type="text" name="nickname" value="<?=@$E["info"]["nickname"]?>"><br> 
-	E-mail: <input type="email" name="email" value="<?=@$E["info"]["email"]?>"><br>
-	<input type="submit" value="edit">
+	<div class="row uniform 50%">
+		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Account:</div><div class="3u 12u$(xsmall)"><?=@$E["info"]["account"]?></div><div class="4u$ 12u$(xsmall)"></div>
+		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Password:</div><div class="3u 12u$(xsmall)"><input type="password" name="password"></div><div class="4u$ 12u$(xsmall)"></div>
+		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Confirm:</div><div class="3u 12u$(xsmall)"><input type="password" name="password2"></div><div class="4u$ 12u$(xsmall)"></div>
+		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Nickname:</div><div class="3u 12u$(xsmall)"><input type="text" name="nickname" value="<?=@$E["info"]["nickname"]?>"></div><div class="4u$ 12u$(xsmall)"></div>
+		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">E-mail:</div><div class="3u 12u$(xsmall)"><input type="email" name="email" value="<?=@$E["i</div>nfo"]["email"]?>"></div><div class="4u$ 12u$(xsmall)"></div>
+		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)"></div><div class="3u 12u$(xsmall)"><input type="submit" value="edit"></div><div class="4u$ 12u$(xsmall)"></div>
+	</div>
 </form>
 </center>
 
