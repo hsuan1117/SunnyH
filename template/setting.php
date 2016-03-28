6d3b513232dfefe6069f481d28ead80456e2cfa3
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-28 20:56:43 +0800

    display style

--- template/setting.php
+++ template/setting.php
@@ -6,15 +6,16 @@ if(!defined("IN_SYSTEM"))
 <?php require("header.php"); ?>
 
 <center>
+<h3>Setting</h3>
 <form method="POST">
 	<div class="row uniform 50%">
-		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Account:</div><div class="3u 12u$(xsmall)"><?=@$E["info"]["account"]?></div><div class="4u$ 12u$(xsmall)"></div>
-		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Password:</div><div class="3u 12u$(xsmall)"><input type="password" name="password"></div><div class="4u$ 12u$(xsmall)"></div>
-		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Confirm:</div><div class="3u 12u$(xsmall)"><input type="password" name="password2"></div><div class="4u$ 12u$(xsmall)"></div>
-		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Nickname:</div><div class="3u 12u$(xsmall)"><input type="text" name="nickname" value="<?=@$E["info"]["nickname"]?>"></div><div class="4u$ 12u$(xsmall)"></div>
-		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Realname:</div><div class="3u 12u$(xsmall)"><input type="text" name="realname" value="<?=@$E["info"]["realname"]?>"></div><div class="4u$ 12u$(xsmall)"></div>
-		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">E-mail:</div><div class="3u 12u$(xsmall)"><input type="email" name="email" value="<?=@$E["info"]["email"]?>"></div><div class="4u$ 12u$(xsmall)"></div>
-		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Facebook:</div><div class="3u 12u$(xsmall)">
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Account:</div><div class="7u 12u$(xsmall)"><?=@$E["info"]["account"]?></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Password:</div><div class="7u 12u$(xsmall)"><input type="password" name="password" placeholder="At least 6 characters." pattern=".{6,}" /></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Confirm:</div><div class="7u 12u$(xsmall)"><input type="password" name="password2" placeholder="Re-enter your password."></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Nickname:</div><div class="7u 12u$(xsmall)"><input type="text" name="nickname" required value="<?=@$E["info"]["nickname"]?>" placeholder="Everyone will identify you from your nickname."></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Realname:</div><div class="7u 12u$(xsmall)"><input type="text" name="realname" required value="<?=@$E["info"]["realname"]?>" placeholder="Enter your real Chinese name. You will be blocked because of fake name."></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Email:</div><div class="7u 12u$(xsmall)"><input type="email" name="email" required value="<?=@$E["info"]["email"]?>" placeholder="Enter your email address, and you can receive the mail."></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Facebook:</div><div class="7u 12u$(xsmall)">
 			<?php
 			if(@$E["info"]["fbid"]==""){
 				?><a href="<?php echo htmlspecialchars($E["info"]["fbloginurl"]); ?>" class="button alt icon fa-facebook">connect</a><?php
@@ -22,8 +23,8 @@ if(!defined("IN_SYSTEM"))
 				echo $E["info"]["fbname"]; ?> <a href="connect.php?disconnect" class="button alt icon fa-facebook">disconnect</a><?php
 			}
 			?>
-		</div><div class="4u$ 12u$(xsmall)"></div>
-		<div class="4u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)"></div><div class="3u 12u$(xsmall)"><input type="submit" value="edit"></div><div class="4u$ 12u$(xsmall)"></div>
+		</div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)"></div><div class="7u 12u$(xsmall)"><input type="submit" value="edit"></div><div class="2u$ 12u$(xsmall)"></div>
 	</div>
 </form>
 </center>
--- template/signup.php
+++ template/signup.php
@@ -5,33 +5,19 @@ if(!defined("IN_SYSTEM"))
 
 <?php require("header.php"); ?>
 
+<center>
 <h3>Signup</h3>
 <form method="POST">
 	<div class="row uniform 50%">
-		<div class="12u 12u$(xsmall)">
-			<input type="text" name="account" required value="<?=@$E["POST"]["account"]?>" placeholder="Account" />
-		</div>
-		<div class="12u$ 12u$(xsmall)">
-			<input type="password" name="password" required placeholder="Password" />
-		</div>
-		<div class="12u$ 12u$(xsmall)">
-			<input type="password" name="password2" required placeholder="Confirm" />
-		</div>
-		<div class="12u$ 12u$(xsmall)">
-			<input type="text" name="nickname" required value="<?=@$E["POST"]["nickname"]?>" placeholder="Nickname" />
-		</div>
-		<div class="12u$ 12u$(xsmall)">
-			<input type="text" name="realname" required value="<?=@$E["POST"]["realname"]?>" placeholder="Realname" />
-		</div>
-		<div class="12u$ 12u$(xsmall)">
-			<input type="email" name="email" required value="<?=@$E["POST"]["email"]?>" placeholder="E-mail" />
-		</div>
-		<div class="12u$">
-			<ul class="actions">
-				<li><input type="submit" value="signup" class="special" /></li>
-			</ul>
-		</div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Account:</div><div class="7u 12u$(xsmall)"><input type="text" name="account" required value="<?=@$E["POST"]["account"]?>" placeholder="Between 3 and 32 alphabetic characters or numerical digits. Cannot be changed." pattern="[A-Za-z0-9]{3,32}" maxlength="32"></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Password:</div><div class="7u 12u$(xsmall)"><input type="password" name="password" required placeholder="At least 6 characters." pattern=".{6,}" /></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Confirm:</div><div class="7u 12u$(xsmall)"><input type="password" name="password2" required placeholder="Re-enter your password."></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Nickname:</div><div class="7u 12u$(xsmall)"><input type="text" name="nickname" required value="<?=@$E["POST"]["nickname"]?>" placeholder="Everyone will identify you from your nickname."></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Realname:</div><div class="7u 12u$(xsmall)"><input type="text" name="realname" required value="<?=@$E["POST"]["realname"]?>" placeholder="Enter your real Chinese name. You will be blocked because of fake name."></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)">Email:</div><div class="7u 12u$(xsmall)"><input type="email" name="email" required value="<?=@$E["POST"]["email"]?>" placeholder="Enter your email address, and you can receive the mail."></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)"></div><div class="7u 12u$(xsmall)"><input type="submit" value="signup" class="special"></div><div class="2u$ 12u$(xsmall)"></div>
 	</div>
 </form>
+</center>
 
 <?php require("footer.php"); ?>
