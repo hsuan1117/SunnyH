f41066d22ee95f2efd77eb902322621fcaa81111
xiplus <huangxuanyuxiplus@gmail.com>
2016-04-30 13:13:32 +0800

    Account begins with a letter

--- template/signup.php
+++ template/signup.php
@@ -10,7 +10,7 @@ if(!defined("IN_SYSTEM"))
 <h3><?php echo _("Signup");?></h3>
 <form method="POST">
 	<div class="row uniform 50%">
-		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)"><?php echo _("Account");?>:</div><div class="7u 12u$(xsmall)"><input type="text" name="account" required value="<?=@$E["POST"]["account"]?>" placeholder="<?php echo _("signup_Account_placeholder");?>" pattern="[A-Za-z0-9]{3,32}" maxlength="32"></div><div class="2u$ 12u$(xsmall)"></div>
+		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)"><?php echo _("Account");?>:</div><div class="7u 12u$(xsmall)"><input type="text" name="account" required value="<?=@$E["POST"]["account"]?>" placeholder="<?php echo _("signup_Account_placeholder");?>" pattern="[A-Za-z][A-Za-z0-9]{2,31}" maxlength="32"></div><div class="2u$ 12u$(xsmall)"></div>
 		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)"><?php echo _("Password");?>:</div><div class="7u 12u$(xsmall)"><input type="password" name="password" required placeholder="<?php echo _("Password_placeholder");?>" pattern=".{6,}" /></div><div class="2u$ 12u$(xsmall)"></div>
 		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)"><?php echo _("Confirm");?>:</div><div class="7u 12u$(xsmall)"><input type="password" name="password2" required placeholder="<?php echo _("Confirm_placeholder");?>"></div><div class="2u$ 12u$(xsmall)"></div>
 		<div class="2u 12u$(xsmall)"></div><div class="1u 12u$(xsmall)"><?php echo _("Nickname");?>:</div><div class="7u 12u$(xsmall)"><input type="text" name="nickname" required value="<?=@$E["POST"]["nickname"]?>" placeholder="<?php echo _("Nickname_placeholder");?>"></div><div class="2u$ 12u$(xsmall)"></div>
