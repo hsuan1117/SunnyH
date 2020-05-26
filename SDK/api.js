8f919e251fd1acb36f437700e8923579dd72f809
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-05-26 22:00:12 +0800

    Update api.js

--- SDK/api.js
+++ SDK/api.js
@@ -1,13 +1,12 @@
 class Sunny {
     
-    _callApi(method,callback){
+    #_callApi(method,callback){
         fetch(this.endpoint+method).then(res=>res.json()).then(callback)
     }
     constructor(endpoint){
         if(endpoint&&endpoint[endpoint.length-1]=="/")endpoint=endpoint.substr(0, endpoint.length-1);
         this.endpoint = endpoint || "https://yiarashi.com/user7/login/api";
-        
-        
+       
         
     }
 }
